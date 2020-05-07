b362a71a5ecf4e20ee157e8cec280d4b8a2295e3
hidjlove1 <61003736+hidjlove1@users.noreply.github.com>
2020-05-07 15:40:32 +0900

    Initial commit

---
+++ app/src/main/java/com/example/alarm_dia/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.alarm_dia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView TextView_now;
+    Button Button_21;
+    Button Button_22;
+    Button Button_23;
+    Button Button_24;
+    Button Button_25;
+    Button Button_26;
+    Button Button_27;
+    Button Button_28;
+    Button Button_29;
+    Button Button_30;
+    Button Button_31;
+    Button Button_32;
+    Button Button_33;
+    Button Button_34;
+    Button Button_35;
+    Button Button_36;
+    Button Button_37;
+    Button Button_38;
+    Button Button_39;
+    Button Button_40;
+    Button Button_41;
+    Button Button_42;
+    Button Button_43;
+    Button Button_44;
+    Button Button_45;
+    Button Button_46;
+    Button Button_47;
+    Button Button_48;
+    Button Button_49;
+    Button Button_50;
+    Button Button_51;
+    Button Button_52;
+    Button Button_53;
+    Button Button_54;
+    Button Button_55;
+    Button Button_56;
+    Button Button_57;
+    Button Button_58;
+    Button Button_59;
+    Button Button_60;
+    Button Button_61;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView_now = findViewById(R.id.textView_now)
+
+        Button_21 = findViewById(R.id.button_21);
+        Button_22 = findViewById(R.id.button_22);
+        Button_23 = findViewById(R.id.button_23);
+        Button_24 = findViewById(R.id.button_24);
+        Button_25 = findViewById(R.id.button_25);
+        Button_26 = findViewById(R.id.button_26);
+        Button_27 = findViewById(R.id.button_27);
+        Button_28 = findViewById(R.id.button_28);
+        Button_29 = findViewById(R.id.button_29);
+        Button_30 = findViewById(R.id.button_30);
+        Button_31 = findViewById(R.id.button_31);
+        Button_32 = findViewById(R.id.button_32);
+        Button_33 = findViewById(R.id.button_33);
+        Button_34 = findViewById(R.id.button_34);
+        Button_35 = findViewById(R.id.button_35);
+        Button_36 = findViewById(R.id.button_36);
+        Button_37 = findViewById(R.id.button_37);
+        Button_38 = findViewById(R.id.button_38);
+        Button_39 = findViewById(R.id.button_39);
+        Button_40 = findViewById(R.id.button_40);
+        Button_41 = findViewById(R.id.button_41);
+        Button_42 = findViewById(R.id.button_42);
+        Button_43 = findViewById(R.id.button_43);
+        Button_44 = findViewById(R.id.button_44);
+        Button_45 = findViewById(R.id.button_45);
+        Button_46 = findViewById(R.id.button_46);
+        Button_47 = findViewById(R.id.button_47);
+        Button_48 = findViewById(R.id.button_48);
+        Button_49 = findViewById(R.id.button_49);
+        Button_50 = findViewById(R.id.button_50);
+        Button_51 = findViewById(R.id.button_51);
+        Button_52 = findViewById(R.id.button_52);
+        Button_53 = findViewById(R.id.button_53);
+        Button_54 = findViewById(R.id.button_54);
+        Button_55 = findViewById(R.id.button_55);
+        Button_56 = findViewById(R.id.button_56);
+        Button_57 = findViewById(R.id.button_57);
+        Button_58 = findViewById(R.id.button_58);
+        Button_59 = findViewById(R.id.button_59);
+        Button_60 = findViewById(R.id.button_60);
+        Button_61 = findViewById(R.id.button_61);
+    }
+}
